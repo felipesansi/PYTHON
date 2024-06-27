94b10580521f39bcc8119c958c81dfa3323c8a95
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-06-27 16:18:41 -0300

    first commit

---
+++ Aula 1/aula 1.py
@@ -0,0 +1,15 @@
+print("Olá, mundo!")
+n =10 #int
+n_real =14.5 #float
+notacao_cientifica = 10.4e+2 #notaçao ciêntifica
+n_complexo = 30.5j; #número complexo
+mensagem = "A mensagem foi enviada!"
+#--------------
+print(n)
+print(n_real)
+print(n_complexo)
+print(mensagem)
+print("\n") #pular linha
+#--------
+nome,idade, email = "Felipe F.", 27, "felipesansi@gmail.com" #atribuido tudo na mesma linha
+print("Meu nome é : ",nome ,"tenho ",idade ,"anos ", "meu e-mail é: ", email)
