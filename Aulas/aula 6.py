3f400f60597da948e615cf5c3b1052bf5150d0aa
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-07-06 17:06:41 -0300

    aula 6

---
+++ Aulas/aula 6.py
@@ -0,0 +1,8 @@
+# Estrutura de dados
+
+lista_frutas = ["Banana", "limão", "maçã", "morango"]
+print(type(lista_frutas));
+
+lista_cursos = ["ADM", "ADS", "FISIO"];
+lista_notas = [10.00, 7.0, 5.8]
+print(lista_cursos+"\n"+lista_notas)
